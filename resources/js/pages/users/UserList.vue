0e47ad6a50cd3ed80d53ea55f2bfe30ff9af0bb5
prismtechbd3 <prismtechb@gmail.com>
2024-05-15 20:52:27 +0600

    complete user form validate

--- resources/js/pages/users/UserList.vue
+++ resources/js/pages/users/UserList.vue
@@ -1,122 +1,150 @@
 <script setup>
 import axios from "axios";
-import { onMounted, reactive, ref } from "vue";
+import { onMounted,  ref } from "vue";
+import { Field} from 'vee-validate';
+import { Form} from 'vee-validate';
+import * as yup from 'yup';
 
 const users = ref([]);
 
-const form = reactive({
-  name: "",
-  email: "",
-  password: ""
+const getUsers = () => {
+    axios.get("/api/users").then((response) => {
+        users.value = response.data;
+    });
+};
+
+const schema = yup.object({
+name : yup.string().required(),
+email : yup.string().email().required(),
+password : yup.string().required().min(8),
 });
 
-const getUsers = () => {
-  axios.get("/api/users").then((response) => {
-    users.value = response.data;
-  });
-};
-
-const createUser = () => {
-  axios.post('/api/users', form).then((response) => {
-    
-    users.value.unshift(response.data);
-    // Clear the form
-
-    form.name = "";
-    form.email = "";
-    form.password = "";
-
-    $('#createUserModal').modal('hide');
-    // Close the modal manually
-    // const modal = document.getElementById('createUserModal');
-    // const bootstrapModal = bootstrap.Modal.getInstance(modal);
-    // bootstrapModal.hide();
-    // Refresh the user list
-    // getUsers();
-  });
-};
+const createUser = (values, {resetForm}) => {
+  axios.post("/api/users", values)
+    .then((response) => {
+      users.value.unshift(response.data);
+      $("#createUserModal").modal("hide"); 
+      resetForm();
+    });
+}
 
 onMounted(() => {
-  getUsers();
+    getUsers();
 });
 </script>
 
 <template>
-  <div class="container mt-4">
-    <!-- Button trigger modal -->
-    <button
-      type="button"
-      class="btn btn-primary mb-3"
-      data-bs-toggle="modal"
-      data-bs-target="#createUserModal"
-    >
-      Create User
-    </button>
+    <div class="container mt-4">
+        <!-- Button trigger modal -->
+        <button
+            type="button"
+            class="btn btn-primary mb-3"
+            data-bs-toggle="modal"
+            data-bs-target="#createUserModal"
+        >
+            Create User
+        </button>
 
-    <!-- Modal -->
-    <div class="card">
-      <div class="card-body">
-        <table class="table table-striped table-bordered">
-          <thead>
-            <tr>
-              <th scope="col">Sr</th>
-              <th scope="col">Name</th>
-              <th scope="col">Email</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr v-for="(user, index) in users" :key="user.id">
-              <th scope="row">{{ index + 1 }}</th>
-              <td>{{ user.name }}</td>
-              <td>{{ user.email }}</td>
-            </tr>
-          </tbody>
-        </table>
-      </div>
-    </div>
-
-    <div
-      class="modal fade"
-      id="createUserModal"
-      tabindex="-1"
-      aria-labelledby="exampleModalLabel"
-      aria-hidden="true"
-    >
-      <div class="modal-dialog">
-        <div class="modal-content">
-          <div class="modal-header">
-            <h5 class="modal-title" id="exampleModalLabel">User Information</h5>
-            <button
-              type="button"
-              class="btn-close"
-              data-bs-dismiss="modal"
-              aria-label="Close"
-            ></button>
-          </div>
-          <form @submit.prevent="createUser" autocomplete="off">
-            <div class="modal-body">
-              <div class="mb-3">
-                <label for="name" class="form-label">Name</label>
-                <input v-model="form.name" type="text" class="form-control" id="name" />
-              </div>
-              <div class="mb-3">
-                <label for="email" class="form-label">Email</label>
-                <input v-model="form.email" type="email" class="form-control" id="email" />
-              </div>
-              <div class="mb-3">
-                <label for="password" class="form-label">Password</label>
-                <input v-model="form.password" type="password" class="form-control" id="password" />
-              </div>
+        <!-- Modal -->
+        <div class="card">
+            <div class="card-body">
+                <table class="table table-striped table-bordered">
+                    <thead>
+                        <tr>
+                            <th scope="col">Sr</th>
+                            <th scope="col">Name</th>
+                            <th scope="col">Email</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr v-for="(user, index) in users" :key="user.id">
+                            <th scope="row">{{ index + 1 }}</th>
+                            <td>{{ user.name }}</td>
+                            <td>{{ user.email }}</td>
+                        </tr>
+                    </tbody>
+                </table>
             </div>
-            <div class="modal-footer">
-              <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-              <button type="submit" class="btn btn-primary">Save changes</button>
-            </div>
-          </form>
         </div>
-      </div>
+
+        <div
+            class="modal fade"
+            id="createUserModal"
+            tabindex="-1"
+            aria-labelledby="exampleModalLabel"
+            aria-hidden="true"
+        >
+            <div class="modal-dialog">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h5 class="modal-title" id="exampleModalLabel">
+                            User Information
+                        </h5>
+                        <button
+                            type="button"
+                            class="btn-close"
+                            data-bs-dismiss="modal"
+                            aria-label="Close"
+                        ></button>
+                    </div>
+                    <!-- form start -->
+                    <Form @submit="createUser" :validation-schema="schema" v-slot="{errors}">
+                        <div class="modal-body">
+                            <div class="mb-3">
+                                <label for="name" class="form-control" :class="{'is-invalid' : errors.name}"
+                                    >Name</label
+                                >
+                                <Field
+                                    name="name"
+                                    type="text"
+                                    class="form-control"
+                                    id="name"
+                                />
+                                <span class="invalid-feedback">{{ errors.name}}</span>
+                            </div>
+                            <div class="mb-3">
+                                <label for="email" class="form-control" :class="{'is-invalid' : errors.email}"
+                                    >Email</label
+                                >
+                                <Field
+                                    name="email"
+                                    type="email"
+                                    class="form-control"
+                                    id="email"
+                                />
+                                <span class="invalid-feedback">{{ errors.email}}</span>
+                            </div>
+                            <div class="mb-3">
+                                <label for="password" class="form-control" :class="{'is-invalid' : errors.password}"
+                                    >Password</label
+                                >
+                                <Field
+                                    name="password"
+                                    type="password"
+                                    class="form-control"
+                                    id="password"
+                                />
+                                <span class="invalid-feedback">{{ errors.password}}</span>
+                            </div>
+                        </div>
+                        <div class="modal-footer">
+                            <button
+                                type="button"
+                                class="btn btn-secondary"
+                                data-bs-dismiss="modal"
+                            >
+                                Close
+                            </button>
+                            <button type="submit" class="btn btn-primary">
+                                Save changes
+                            </button>
+                        </div>
+                    </Form>
+                    <!-- End form  -->
+                </div>
+            </div>
+        </div>
     </div>
-  </div>
 </template>
 
 <style>
