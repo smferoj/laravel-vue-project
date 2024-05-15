5f0e6de8e9d36dd62f3f382374d2cdabcf103c68
smferoj <sm.feroj@gmail.com>
2024-05-15 10:54:57 +0600

    continue for add

--- resources/js/pages/users/UserList.vue
+++ resources/js/pages/users/UserList.vue
@@ -7,19 +7,6 @@ import { onMounted, ref } from 'vue';
 const users = ref([]);
 
 
-// const users = [
-//   {
-//   id:1,
-//   name:'John Doe',
-//   email: 'doe@gmail.com'
-// },
-//   {
-//   id:2,
-//   name:'John poe',
-//   email: 'poe@gmail.com'
-// },
-// ];
-
 
 const getUsers = () => {
   axios.get('/api/users')
@@ -36,6 +23,36 @@ onMounted(()=>{
 
 <template>
   <div class="container mt-4">
+
+    <!-- Button trigger modal -->
+<button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
+  Launch demo modal
+</button>
+
+<!-- Modal -->
+<div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+        ...
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-primary">Save changes</button>
+      </div>
+    </div>
+  </div>
+</div>
+
+
+    <div class="card">
+      <div class="card-body">
+
+ 
     <table class="table table-striped table-bordered">
       <thead>
         <tr>
@@ -53,6 +70,9 @@ onMounted(()=>{
         </tr>
       </tbody>
     </table>
+
+  </div>
+    </div>
   </div>
 </template>
 
